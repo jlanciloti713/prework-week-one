0efd61f045e03593c4405d7ad2e687040298a944
Jack Lanciloti <jlanciloti713@gmail.com>
2018-09-19 21:17:17 -0500

    create evaluated_expressions

---
+++ evaluated_expressions.rb
@@ -0,0 +1,9 @@
+puts 3 + 3 + 3 * 10 == 36
+puts (2 * 4) / 2 - 4 == 0
+puts "hello" * 3 == "hellohellohello"
+puts "hello" + "3" == "hello3"
+puts !true == false
+puts "false" == "false"
+puts true && true
+puts false || true
+puts false ||true && 3 == 2 + 1
