4233ad0dea5ae7a67305e896d76a9e7360e5a9df
Jack Lanciloti <jlanciloti713@gmail.com>
2018-09-20 20:53:11 -0500

    convert_to_fahrenheit.rb

--- convert_to_fahrenheit.rb
+++ convert_to_fahrenheit.rb
@@ -2,8 +2,7 @@ def convert_to_fahrenheit(celsius)
 puts celsius * 1.8 + 32
 end
 
-convert_to_fahrenheit(4)
-convert_to_fahrenheit(45)
-convert_to_fahrenheit(200)
+convert_to_fahrenheit(5)
 convert_to_fahrenheit(0)
-convert_to_fahrenheit(-3)
+convert_to_fahrenheit(200)
+convert_to_fahrenheit(-7)
