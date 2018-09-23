268751b9af959e923387c7fec0d9826936cfa18f
Jack Lanciloti <jlanciloti713@gmail.com>
2018-09-23 11:50:03 -0500

    Created hashes.rb

---
+++ hashes.rb
@@ -0,0 +1,16 @@
+#my_hash = { "first_name" => "Jack", "age" => 23, "last_name" => "Lanciloti"}
+
+#my_hash.each { |key, value| puts value; puts key }
+
+#my_hash.each do |key, value|
+ # puts value
+#end
+
+#puts my_hash.select { |key, value| value.to_s.include?("a") }
+#puts my_hash.reject { |key, value| value.to_s.include?("a") }
+
+:hello_world
+
+my_hash = { first_name: "Jack", age: 23, last_name: "Lanciloti" }
+puts my_hash[:first_name]
+puts my_hash[:age]
