3032440e9c5187ade38bf01e39130df712a0552b
Jack Lanciloti <jlanciloti713@gmail.com>
2018-09-22 23:42:55 -0500

    Create loops.rb

---
+++ loops.rb
@@ -0,0 +1,33 @@
+#15.times do
+ # puts "Hello"
+#end
+
+#counter = 0
+
+#while counter < 15
+ # puts "hello"
+  #counter += 1
+#end
+
+#puts "the loopydoop"
+
+#[1, 2, 3, 4, 5, "jello"].each do |value|
+ # puts value
+#end
+
+#array = [1, 2, 3, 4, 5, "jello"]
+
+#index = 0
+
+#array.length.times do
+ # puts array [index]
+  #index += 1
+#end
+
+array = [1, 2, 3, 4, 5, "jello"]
+index = 0
+
+while index < array.length
+  puts array[index]
+  index += 1
+end
--- number_incrementor.rb
+++ number_incrementor.rb
@@ -1,14 +1,8 @@
-def increment(number)
-  puts number
-  puts number + 1
-  puts number + 2
-  puts number + 3
-  puts number + 4
-  puts number + 5
-  puts number + 6
-  puts number + 7
-  puts number + 8
-  puts number + 9
+def increment(number, number_of_iterations)
+  number_of_iterations.times do
+    puts number
+    number += 1
+  end
 end
 
-increment(42)
+increment(42, 10000)
