12feecc8eecec31d6cb4691688f46293496a146c
Jack Lanciloti <jlanciloti713@gmail.com>
2018-09-23 21:01:51 -0500

    Refactor create_person.rb

--- create_person.rb
+++ create_person.rb
@@ -1,4 +1,10 @@
-def create_person
- hash = { first_name: "John", last_name: "Doe", age: 25 }
+# def create_person
+#  hash = { first_name: "John", last_name: "Doe", age: 25 }
+# end
+# puts create_person
+
+def create_person(first_name, last_name, age)
+      hash = { first_name: first_name, last_name: last_name, age: age }
 end
-puts create_person
+
+puts create_person("John", "Doe", 25)
